616c1845810443f6e1c30ce4a2a43200a8c5c089
nguyen tuan dang <nguyentuandang@Mac-mini.local>
2017-05-29 18:27:51 +0700

    dsad

---
+++ SDKTest3/SDKTest3.h
@@ -0,0 +1,19 @@
+//
+//  SDKTest3.h
+//  SDKTest3
+//
+//  Created by nguyen tuan dang on 5/29/17.
+//  Copyright © 2017 Zi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKTest3.
+FOUNDATION_EXPORT double SDKTest3VersionNumber;
+
+//! Project version string for SDKTest3.
+FOUNDATION_EXPORT const unsigned char SDKTest3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKTest3/PublicHeader.h>
+
+
