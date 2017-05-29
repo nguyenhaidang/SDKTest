3c8a2ccbe997129d5a502c763cda4fa3b1b1c377
nguyen tuan dang <nguyentuandang@Mac-mini.local>
2017-05-29 17:49:18 +0700

    dfsdf

---
+++ SDKTest2/SDKTest2.h
@@ -0,0 +1,19 @@
+//
+//  SDKTest2.h
+//  SDKTest2
+//
+//  Created by nguyen tuan dang on 5/29/17.
+//  Copyright © 2017 Zi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKTest2.
+FOUNDATION_EXPORT double SDKTest2VersionNumber;
+
+//! Project version string for SDKTest2.
+FOUNDATION_EXPORT const unsigned char SDKTest2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKTest2/PublicHeader.h>
+
+
